138a0bd4d8e13b812bf1e223b18952c9669f957b
Gabriel <gabriel.bachelor@gmail.com>
2025-01-15 19:44:24 +0000

    x

--- pern/src/App.tsx
+++ pern/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
   useEffect(() => {
     const getText = async () => {
       //to be able to use async and await in useEffect we need to create an async function.
-      const response = await fetch("http://localhost:5001"); //we fetch from our api server running on port 5001
+      const response = await fetch("http://database.cdqmgmgm25g1.eu-north-1.rds.amazonaws.com:5001"); //we fetch from our api server running on port 5001
       const data = await response.json(); //we get the json data
       //the data is an array with one item. This item is an object with _id and greeting properties
       //this makes sense since we turned the data into an array in line 20 in server.js and the array is the collection from MongoDB
